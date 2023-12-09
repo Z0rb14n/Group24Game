93419b17e12dcd505b62055721a4f16084ec9e34
Jonathan Nah <48940672+Z0rb14n@users.noreply.github.com>
2023-12-08 20:02:03 -0800

    Preliminary Inventory System (#8)
    * LMAOOOOO THIS IS SCUFFED
    
    * BRO UNREAL ENGINE STOP NUKING MY CHANGES
    
    * Test Create new Map lol

--- Source/Group24Game/Group24GameGameMode.h
+++ Source/Group24Game/Group24GameGameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Private/FUInventoryItem.h"
 #include "Group24GameGameMode.generated.h"
 
 UCLASS(minimalapi)
@@ -13,6 +14,8 @@ class AGroup24GameGameMode : public AGameModeBase
 
 public:
 	AGroup24GameGameMode();
+
+	TArray<FUInventoryItem> CurrentInventory;
 };
 
 
---
+++ Source/Group24Game/Private/TPItemPickupComponent.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "FUInventoryItem.h"
+#include "Group24Game/Group24GameCharacter.h"
+
+#include "TPItemPickupComponent.generated.h"
+
+UCLASS(Blueprintable, BlueprintType, meta = (BlueprintSpawnableComponent))
+class UTPItemPickupComponent : public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+	FUInventoryItem Item;
+	
+	UFUNCTION(BlueprintCallable)
+	void AddItem(AGroup24GameCharacter* TargetCharacter) const;
+};
